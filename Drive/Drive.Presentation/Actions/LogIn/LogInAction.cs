1d4e694d702565492b15ca5e2ca43e88a1433f60
ViceElez <vice.elezovic12@gmail.com>
2024-12-23 02:00:49 +0100

    added login and signin functions to presentation layer

--- Drive/Drive.Presentation/Actions/LogIn/LogInAction.cs
+++ Drive/Drive.Presentation/Actions/LogIn/LogInAction.cs
@@ -8,6 +8,94 @@ namespace Drive.Presentation.Actions.LogIn
 {
     public class LogInAction
     {
+        public static void UserLogInAction()
+        {
+            Console.Write("Upisite email za login:");
+            var loginEmail = Console.ReadLine().Trim();
+            while (true)
+            {
+                if (string.IsNullOrEmpty(loginEmail))
+                {
+                    Console.WriteLine("Email nemoze biti prazan.");
+                    var confirmForEmail = Helper.InputValidation.ConfirmAndDelete();
+                    if (confirmForEmail)
+                    {
+                        Console.Write("Unesite email:");
+                        loginEmail = Console.ReadLine().Trim();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Proces logina je prekinut.");
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                else if (!Helper.InputValidation.IsValid(loginEmail))
+                {
+                    Console.WriteLine("Neipsravan format email-a.");
+                    var confirmForEmail = Helper.InputValidation.ConfirmAndDelete();
+                    if (confirmForEmail)
+                    {
+                        Console.Write("Unesite email:");
+                        loginEmail = Console.ReadLine().Trim();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Proces logina je prekinut.");
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                else if (false)//samo u domain napravi za provejeru
+                {
+                    Console.WriteLine("Ne postoji racun s tim emailom");
+                    var confirmForEmail = Helper.InputValidation.ConfirmAndDelete();
+                    if (confirmForEmail)
+                    {
+                        Console.Write("Unesite email za login:");
+                        loginEmail = Console.ReadLine().Trim();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Proces logina je prekinut.");
+                        Console.ReadKey();
+                        return;
+                    }
 
+                }
+                else
+                    break;
+            }
+
+            Console.Write("Upisite lozinku:");
+            var loginPassword = Console.ReadLine().Trim();
+            while (true)
+            {
+                if (string.IsNullOrEmpty(loginPassword))
+                {
+                    Console.WriteLine("Lozinka nemoze biti prazna.");
+                    var confirmForPassword = Helper.InputValidation.ConfirmAndDelete();
+                    if (confirmForPassword)
+                    {
+                        Console.Write("Unesite lozinku:");
+                        loginPassword = Console.ReadLine().Trim();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Proces logina je prekinut.");
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                else
+                    break;
+            }
+
+            //provjeri u domainu
+            //ako je sve ok onda se prikaze menu
+            Console.WriteLine("Uspjesno logirani.");
+            Console.ReadKey();
+            Drive.Presentation.Actions.Menus.DriveMenuActions.DriveMenu();
+        }
     }
 }
--- Drive/Drive.Presentation/Actions/SignIn/SignInAction.cs
+++ Drive/Drive.Presentation/Actions/SignIn/SignInAction.cs
@@ -1,1 +1,145 @@
 ﻿
+namespace Drive.Presentation.Actions.SignIn
+{
+    public class SignInAction
+    {
+        public static void UserSignInAction()
+        {
+            Console.Write("Upisite mail za registraciju:");
+            var registrationEmail = Console.ReadLine().Trim();
+            while (true)
+            {
+                if (string.IsNullOrEmpty(registrationEmail))
+                {
+                    Console.WriteLine("Email nemoze biti prazan.");
+                    var confirmForEmail = Helper.InputValidation.ConfirmAndDelete();
+                    if (confirmForEmail)
+                    {
+                        Console.Write("Unesite email:");
+                        registrationEmail = Console.ReadLine().Trim();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Proces registracije je prekinut.");
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                else if (!Helper.InputValidation.IsValid(registrationEmail))
+                {
+                    Console.WriteLine("Neipsravan format email-a.");
+                    var confirmForEmail = Helper.InputValidation.ConfirmAndDelete();
+                    if (confirmForEmail)
+                    {
+                        Console.Write("Unesite email:");
+                        registrationEmail = Console.ReadLine().Trim();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Proces registracije je prekinut.");
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                else if (false)//samo u domain napravi za provejeru
+                {
+                    Console.WriteLine("Vec postoji racun s tim emailom");
+                    var confirmForEmail = Helper.InputValidation.ConfirmAndDelete();
+                    if (confirmForEmail)
+                    {
+                        Console.Write("Unesite email za registraciju:");
+                        registrationEmail = Console.ReadLine().Trim();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Proces registracije je prekinut.");
+                        Console.ReadKey();
+                        return;
+                    }
+
+                }
+                else
+                    break;
+            }
+
+
+            Console.Write("Upisite lozinku za registraciju:");
+            var registrationPassword = Console.ReadLine().Trim();
+            while (true)
+            {
+                if (string.IsNullOrEmpty(registrationPassword))
+                {
+                    Console.WriteLine("Lozinka nemoze biti prazna.");
+                    var confirmForPassword = Helper.InputValidation.ConfirmAndDelete();
+                    if (confirmForPassword)
+                    {
+                        Console.Write("Unesite lozinku:");
+                        registrationPassword = Console.ReadLine().Trim();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Proces registracije je prekinut.");
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Upisite ponovno lozinku za registraciju:");
+            var registrationPasswordRepeat = Console.ReadLine().Trim();
+            while (true)
+            {
+                if (string.IsNullOrEmpty(registrationPasswordRepeat))
+                {
+                    Console.WriteLine("Lozinka nemoze biti prazna.");
+                    var confirmForPassword = Helper.InputValidation.ConfirmAndDelete();
+                    if (confirmForPassword)
+                    {
+                        Console.Write("Unesite ponovno lozinku:");
+                        registrationPasswordRepeat = Console.ReadLine().Trim();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Proces registracije je prekinut.");
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                else if (registrationPassword != registrationPasswordRepeat)
+                {
+                    Console.WriteLine("Lozinke se ne podudaraju.");
+                    var confirmForPassword = Helper.InputValidation.ConfirmAndDelete();
+                    if (confirmForPassword)
+                    {
+                        Console.Write("Unesite ponovno lozinku:");
+                        registrationPasswordRepeat = Console.ReadLine().Trim();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Proces registracije je prekinut.");
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Uspjesno registrirani!");
+            Console.ReadKey();
+
+            //spremi u bazu
+
+            Drive.Presentation.Actions.Menus.DriveMenuActions.DriveMenu();
+
+
+
+        }
+    }
+}
